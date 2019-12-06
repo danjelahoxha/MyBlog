416fd1002f4c9ee81a6f9aa4ead7f2ac6e8ecf72
danjela <danjelashehi@gmail.com>
2019-12-06 10:15:23 +0100

    Aded React and most basic components

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class MainContainer extends React.Component {
+  state = {
+    cards: []
+  };
+
+  render() {
+    return <div className="main-container">just test</div>;
+  }
+}
